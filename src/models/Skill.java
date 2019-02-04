9a50af051c7aed22e08093bca1ae5c3e0e2bcfde
redboston1995 <44159654+redboston1995@users.noreply.github.com>
2019-02-04 09:01:35 +0000

    Updated Student and Skill classes
    Entered attributes of the student and skill classes, minor update, more to come

---
+++ src/models/Skill.java
@@ -0,0 +1,10 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Skill {
+	private String name;
+	private String description;
+	private int currentRating;
+	private ArrayList<Integer> prevRatings;
+}
--- src/models/Student.java
+++ src/models/Student.java
@@ -1,8 +1,15 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Student {
 	
-	
+	private String firstName;
+	private String lastName;
+	private String course;
+	private String job;
+	private int age;
+	private ArrayList<Skill> skills;
 	
 	@Override
 	public String toString() {
