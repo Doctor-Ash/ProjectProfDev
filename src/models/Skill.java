dc44d4ee49ebd0cbde3db58278ff06e14f19e0cd
redboston1995 <44159654+redboston1995@users.noreply.github.com>
2019-02-04 10:32:37 +0000

    Further update to Student and Skill
    Added methods

--- src/models/Skill.java
+++ src/models/Skill.java
@@ -7,4 +7,50 @@ public class Skill {
 	private String description;
 	private int currentRating;
 	private ArrayList<Integer> prevRatings;
+	private Student student;
+	
+	public Skill(String name, String description, int currentRating, Student s) {
+		this.name = name;
+		this.description = description;
+		this.currentRating = currentRating;
+		this.student = s;
+		
+		prevRatings.add(currentRating);
+		//adds this skill to the student who created it
+		s.addSkill(this);
+	}
+	
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void changeRating(int r) {
+		this.currentRating = r;
+		prevRatings.add(r);
+	}
+	
+	//returns an array list of all the previous ratings for this skill and student
+	public ArrayList<Integer> getRatings() {
+		return prevRatings;
+	}
+	
+	@Override
+	public String toString() {
+		return "Skill [name=" + name + ", description=" + description + ", currentRating=" + currentRating
+				+ ", prevRatings=" + prevRatings + "]";
+	}
 }
--- src/models/Student.java
+++ src/models/Student.java
@@ -4,17 +4,88 @@ import java.util.ArrayList;
 
 public class Student {
 	
-	private String firstName;
-	private String lastName;
-	private String course;
-	private String job;
+	private int stu_id; 
+	private String first_name;
+	private String last_name;
 	private int age;
+	private String email;
+	private String university;
+	private String course;
+	
 	private ArrayList<Skill> skills;
 	
+	public Student(String first_name, String last_name, int age, String email, String university, 
+			String course) {
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.age = age;
+		this.email = email;
+		this.university = university;
+		this.course = course;
+	}
+	
+	//add a skill to the list of skills associated with this student
+	public void addSkill(Skill skill) {
+		skills.add(skill);
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUniversity() {
+		return university;
+	}
+
+	public void setUniversity(String university) {
+		this.university = university;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	
+
 	@Override
 	public String toString() {
-		return "";
+		return "Student [first_name=" + first_name + ", last_name=" + last_name + ", age=" + age + ", email=" + email
+				+ ", university=" + university + ", course=" + course;
 	}
+	
+	
 }
 
 
