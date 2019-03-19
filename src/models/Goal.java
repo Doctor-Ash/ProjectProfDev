1754ea34f91ae6c74214a8c6a222dba5f9ee4a5f
dellaswift2 <44359897+dellaswift2@users.noreply.github.com>
2019-03-19 00:09:11 +0000

    goals

--- src/models/Goal.java
+++ src/models/Goal.java
@@ -11,6 +11,103 @@ public class Goal {
 	String jul;
 	String aug;
 	String sep;
+	
+	
+	public Goal(String jan, String feb, String mar, String apr, String may, String jun, String jul, String aug,
+			String sep, String oct, String nov, String dec, String username) {
+		super();
+		this.jan = jan;
+		this.feb = feb;
+		this.mar = mar;
+		this.apr = apr;
+		this.may = may;
+		this.jun = jun;
+		this.jul = jul;
+		this.aug = aug;
+		this.sep = sep;
+		this.oct = oct;
+		this.nov = nov;
+		this.dec = dec;
+		this.username = username;
+	}
+	public String getJan() {
+		return jan;
+	}
+	public void setJan(String jan) {
+		this.jan = jan;
+	}
+	public String getFeb() {
+		return feb;
+	}
+	public void setFeb(String feb) {
+		this.feb = feb;
+	}
+	public String getMar() {
+		return mar;
+	}
+	public void setMar(String mar) {
+		this.mar = mar;
+	}
+	public String getApr() {
+		return apr;
+	}
+	public void setApr(String apr) {
+		this.apr = apr;
+	}
+	public String getMay() {
+		return may;
+	}
+	public void setMay(String may) {
+		this.may = may;
+	}
+	public String getJun() {
+		return jun;
+	}
+	public void setJun(String jun) {
+		this.jun = jun;
+	}
+	public String getJul() {
+		return jul;
+	}
+	public void setJul(String jul) {
+		this.jul = jul;
+	}
+	public String getAug() {
+		return aug;
+	}
+	public void setAug(String aug) {
+		this.aug = aug;
+	}
+	public String getSep() {
+		return sep;
+	}
+	public void setSep(String sep) {
+		this.sep = sep;
+	}
+	public String getOct() {
+		return oct;
+	}
+	public void setOct(String oct) {
+		this.oct = oct;
+	}
+	public String getNov() {
+		return nov;
+	}
+	public void setNov(String nov) {
+		this.nov = nov;
+	}
+	public String getDec() {
+		return dec;
+	}
+	public void setDec(String dec) {
+		this.dec = dec;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
 	String oct;
 	String nov;
 	String dec;
