eacb1b27db78bbc0a8a3905b6a9c008cbab394fd
della <dellaswift2@gmail.com>
2019-03-19 13:31:23 +0000

    addgoal finished

--- src/models/GoalDAO.java
+++ src/models/GoalDAO.java
@@ -33,120 +33,17 @@ public class GoalDAO {
 		return dbConnection;
 	}
 	
-	public ArrayList<Skill> getAllSkills(String username) throws SQLException {
-		Connection dbConnection = null;
-		Statement statement= null;
-		ResultSet result = null;
-		String query = "SELECT * FROM skill WHERE username = '" + username + "';";
-		ArrayList<Skill> skills = new ArrayList<>();
-		
-		try 
-		{
-			dbConnection = getDBConnection();
-			statement = dbConnection.createStatement();
-			System.out.println("DBQuery = " + query);
-			result = statement.executeQuery(query);
-			
-			while(result.next())
-			{
-				String skill_name = result.getString("skill_name");
-				String description = result.getString("description");
-				int rating = result.getInt("rating");
-				String date = result.getString("date");
-				
-				skills.add(new Skill
-						(
-						skill_name,
-						description,
-						rating,
-						username,
-						date
-						));
-			}
-		} catch(Exception e)
-		{
-			System.out.println("get all skills: "+e);
-		} finally 
-		{
-			if (result != null) 
-			{
-				result.close();
-			}
-			if (statement != null) 
-			{
-				statement.close();
-			}
-			if (dbConnection != null) 
-			{
-				dbConnection.close();
-			}
-		}
-		return skills;
-	}
-	/* public ArrayList<Skill> getSkill(String username,String skillPass) throws SQLException  
-	{
-		Connection dbConnection = null;
-		Statement statement = null;
-		ResultSet result = null;
-		ArrayList<Skill> ratings = new ArrayList<>();
-
-		
-		//gets a individual vehicle
-		String query = "SELECT rating, date FROM skill WHERE username = '" + username + "' AND skill_name = '" + skillPass + "';";
-
-		try {
-			dbConnection = getDBConnection();
-			statement = dbConnection.createStatement();
-			System.out.println("DBQuery: " + query);
-			// execute SQL query
-			result = statement.executeQuery(query);
-
-			while (result.next())
-			{
-				//String skill_name = result.getString("skill_name");
-				int rating = result.getInt("rating");
-				String date = result.getString("date");
-
-				ratings.add(new Skill
-						(
-						rating,
-						date
-						));
-				
-			}
-		} catch(Exception e)
-		{
-			System.out.println("get all skills: "+e);
-		} finally 
-		{
-			if (result != null) 
-			{
-				result.close();
-			}
-			if (statement != null) 
-			{
-				statement.close();
-			}
-			if (dbConnection != null) 
-			{
-				dbConnection.close();
-			}
-		}
-		return ratings;
-	}*/
 	
 	
 	
-	public boolean insertSkill(Skill in) throws SQLException
+	
+	public boolean insertGoal(String month, String username, String goal) throws SQLException
 	{
 		Connection dbConnection = null;
 		Statement statement= null;
 		
-		String update = "INSERT INTO skill (skill_name, description, rating, date,"
-				+ " username) VALUES "
-				+ "('"+in.getSkill_name()+"','"+in.getDescription()+"',"
-						+ "'"+in.getRating()+"', datetime('now'), "
-						+ "'"+in.getUsername()+"')";
+		String update = "UPDATE goals Set " + month +" = '" + goal + "';" ;
+				
 		boolean ok = false;
 		
 		try 
